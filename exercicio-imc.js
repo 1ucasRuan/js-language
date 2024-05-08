fdac0602e939ec92c04348724d210524a554627b
1ucasruan <lucasruan1.tuc@gmail.com>
2024-05-08 18:58:34 -0300

    doc: add exemplo preço produto

---
+++ exercicio-imc.js
@@ -0,0 +1,26 @@
+let peso = 60;
+let altura = 1.69;
+
+let imc = peso / (altura * altura);
+
+
+if (imc < 18.5) {
+    console.log('Abaixo do peso');
+
+}
+else if (imc > 18.5 && imc < 25) {
+    console.log('Peso normal');
+
+}
+else if (imc > 25 && imc < 30) {
+    console.log('Acima do peso');
+
+}
+else if (imc > 30 && imc < 40) {
+    console.log('Obeso');
+
+} else {
+    console.log('Obesidade grave');
+
+}
+console.log(imc);
