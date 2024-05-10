240af6dfa07466a5de025b9e89f251399865950a
1ucasruan <lucasruan1.tuc@gmail.com>
2024-05-10 15:07:56 -0300

    doc: add class aula-objetos

---
+++ 06 - Objetos/aula-objeto.js
@@ -0,0 +1,20 @@
+const pessoa = {
+    nome : 'Lucas Ruan da Silva de Carvalho',
+    idade : 24,
+
+    descrever: function (){
+        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`)
+    }
+};
+
+console.log(pessoa);
+pessoa.nome = 'Joice';
+pessoa.idade = 22;
+
+pessoa.descrever();
+//console.log(lucas.nome);
+//console.log(lucas.idade);
+//console.log(lucas);
+//lucas.altura = 1.69;
+//delete lucas.nome;
+//console.log(lucas);
