20e9ca834ecc48a3fbcd78bbdddc816cded06329
1ucasruan <lucasruan1.tuc@gmail.com>
2024-05-14 14:54:31 -0300

    feat: updade class pessoa

--- 06 - Objetos/aula-objeto.js
+++ 06 - Objetos/aula-objeto.js
@@ -16,13 +16,18 @@ class Pessoa {
 
 function compararPessoas(p1, p2){
     if(p1.idade > p2.idade){
-        console.log()
+        console.log(`${p1.nome} é mais velho que ${p2.nome}`);
+    }else if(p2.idade > p1.idade){
+        console.log(`${p2.nome} é mais velho que ${p1.nome}`);
+    }else{
+        console.log(`${p1.nome} e ${p2.nome} tem a mesma idade`);
     }
 }
 
+const joice = new Pessoa('Joice', 22);
+const lucas = new Pessoa('Lucas', 24);
 
-
-
+compararPessoas(joice,lucas);
 
 //const lucas = new Pessoa('Lucas', 24);
 //lucas.nome = 'Lucas Ruan da Silva de Carvalho';
