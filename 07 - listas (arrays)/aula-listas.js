64ebe3ed331e3331fc566a743267275a36e52380
1ucasruan <lucasruan1.tuc@gmail.com>
2024-05-16 15:13:21 -0300

    feat: update aula-listas

--- 07 - listas (arrays)/aula-listas.js
+++ 07 - listas (arrays)/aula-listas.js
@@ -5,16 +5,17 @@ notas.push(7);
 notas.push(8);
 notas.push(2);
 notas.push(5);
+notas.push(8);
 
 console.log(notas.length);
 
 let soma = 0;
 
 for(let i = 0; i < notas.length; i++){
-   const soma = soma + notas[i];
+    soma = soma + notas[i];
     console.log(soma);
 }
 
 console.log(soma);
-let media = soma / notas.length;
+const media = soma / notas.length;
 console.log(media);
