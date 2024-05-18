f7cd0a5b6ea9c8c62b83013506b8e5dd3f19bdbb
1ucasruan <lucasruan1.tuc@gmail.com>
2024-05-18 14:50:14 -0300

    feat: incluindo loop no exercicio-array-maior.js

--- 07 - listas (arrays)/exercicio-array-maior.js
+++ 07 - listas (arrays)/exercicio-array-maior.js
@@ -1,11 +1,15 @@
 function encontrarMaior(numeros){
+
+    let maior = 0;
     for (let i = 0; i < numeros.length; i++) {
         
-        if((numeros[i] % 2) === 0){
-            
-            
+        if(maior < numeros[i]){
+            maior = numeros[i];      
         }        
     }
+    return maior;
 }
 
 const numeros = [1, 2, 3 , 4, 5, 6, 7, 8, 9, 10];//array com os elementos
+
+console.log(encontrarMaior(numeros));
