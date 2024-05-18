b9d993bf82632b129a275ddcb8ac4cb7289a7cfc
1ucasruan <lucasruan1.tuc@gmail.com>
2024-05-18 14:22:40 -0300

    feat: add comentarios no exercicio-numero-par.js

--- 07 - listas (arrays)/exercicio-numero-par.js
+++ 07 - listas (arrays)/exercicio-numero-par.js
@@ -1,6 +1,6 @@
 function encontrarPar(numeros){
     for (let i = 0; i < numeros.length; i++) {
-        //const element = array[i];
+        
         if((numeros[i] % 2) === 0){
             console.log(`${numeros[i]} é par`);
             //console.log(numeros[i]);
@@ -8,8 +8,9 @@ function encontrarPar(numeros){
     }
 }
 
-const numeros = [1, 2, 3 , 4, 5, 6, 7, 8, 9, 10];
+const numeros = [1, 2, 3 , 4, 5, 6, 7, 8, 9, 10];//array com os elementos
 
+//adicionando no final da lista
 numeros.push(11);
 numeros.push(12);
 numeros.push(13);
@@ -17,4 +18,5 @@ numeros.push(14);
 numeros.push(15);
 numeros.push(16);
 
+//chmando funcao para encontrar os numeros pares
 encontrarPar(numeros);
