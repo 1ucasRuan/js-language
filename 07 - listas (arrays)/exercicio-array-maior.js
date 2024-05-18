e39e60606dde230a00c69d665b271982ad3fd38d
1ucasruan <lucasruan1.tuc@gmail.com>
2024-05-18 14:46:38 -0300

    doc: add exercicio-array-maior.js

---
+++ 07 - listas (arrays)/exercicio-array-maior.js
@@ -0,0 +1,11 @@
+function encontrarMaior(numeros){
+    for (let i = 0; i < numeros.length; i++) {
+        
+        if((numeros[i] % 2) === 0){
+            
+            
+        }        
+    }
+}
+
+const numeros = [1, 2, 3 , 4, 5, 6, 7, 8, 9, 10];//array com os elementos
