86edace5c64771d4c2f6948f3936411b1ebf9995
1ucasruan <lucasruan1.tuc@gmail.com>
2024-05-20 14:59:07 -0300

    feat: encontar maior valor funcoes-auxiliares.js

--- 08 - importacao/main.js
+++ 08 - importacao/main.js
@@ -6,8 +6,20 @@
 
 const {gets, print} = require('./funcoes-auxiliares');
 
-print(gets());
-print(gets());
-print(gets());
-print(gets());
-print(gets());
+const numerosSorteados = [];
+
+for (let i = 0; i < 5; i++) {
+    print(gets());
+    const numeroSorteado = gets();   
+    numerosSorteados.push(numeroSorteado); 
+}
+
+let maiorValor = 0;
+for (let i = 0; i < numerosSorteados.length; i++) {
+    const numeroSorteado = numerosSorteados[i];
+    if(numeroSorteado > maiorValor){
+        maiorValor = numeroSorteado
+    }
+}
+
+print(maiorValor);
