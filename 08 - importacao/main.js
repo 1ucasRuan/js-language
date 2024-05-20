636b57530649bf39f43384c66178462ae80ccbb3
1ucasruan <lucasruan1.tuc@gmail.com>
2024-05-20 14:47:56 -0300

    feat: add funcoes gets e print

--- 08 - importacao/main.js
+++ 08 - importacao/main.js
@@ -1,6 +1,13 @@
-const funcoes = require('./funcoes-auxiliares');
+//const funcoes = require('./funcoes-auxiliares');
 
-console.log(funcoes);
+//console.log(funcoes);
 
-console.log(funcoes.gets());
+//console.log(funcoes.gets());
 
+const {gets, print} = require('./funcoes-auxiliares');
+
+print(gets());
+print(gets());
+print(gets());
+print(gets());
+print(gets());
