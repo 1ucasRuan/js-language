14143764a219dfce880bf02df468459935b13088
1ucasruan <lucasruan1.tuc@gmail.com>
2024-05-21 12:33:44 -0300

    doc: add exercicio-media.js

--- 08 - importacao/funcoes-auxiliares.js
+++ 08 - importacao/funcoes-auxiliares.js
@@ -1,5 +1,17 @@
-const entradas = [5, 5, 50, 10, 98, 23];
+/*const entradas = [5, 5, 50, 10, 98, 23];
 let i = 0;
+
+function gets(){
+    const valor = entradas[i];
+    i++;
+    return  valor;
+}
+
+function print(texto){
+    console.log(texto)
+}
+
+module.exports = {gets, print}*/
 
 function gets(){
     const valor = entradas[i];
